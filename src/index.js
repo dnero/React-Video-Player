5831684cd7e36b3c1eeb722b7fe281476cfad169
D'ontreye Nero <dontreyenero@gmail.com>
2017-02-05 14:25:41 -0500

    Convert functional component to a class and set response in the state

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -6,17 +6,26 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCvbCsrYIpt2IHFGw3iJqtE8Iyhvked1ZA';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
-	console.log(data);
-});
 
 // create a new component. it should produce html
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+			super(props);
+
+			this.state = { videos: [] };
+
+			YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+				this.setState({ videos });
+			});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	};
 };
 
 // take this components html and put it ion the page in the dom

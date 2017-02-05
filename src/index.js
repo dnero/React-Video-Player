fa5f622f012a1b5242f95dbb97cd37ecc70454a5
D'ontreye Nero <dontreyenero@gmail.com>
2017-02-05 14:09:26 -0500

    Add sample YouTube search call

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCvbCsrYIpt2IHFGw3iJqtE8Iyhvked1ZA';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
+	console.log(data);
+});
 
 // create a new component. it should produce html
 const App = () => {
@@ -11,7 +16,7 @@ const App = () => {
 		<div>
 			<SearchBar />
 		</div>
-	)
+	);
 };
 
 // take this components html and put it ion the page in the dom

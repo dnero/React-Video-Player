7bc8879819d08390857d95810bdd5a15529fa16c
D'ontreye Nero <dontreyenero@gmail.com>
2017-02-05 16:25:49 -0500

    Implement searchbar term change

--- src/index.js
+++ src/index.js
@@ -20,19 +20,25 @@ class App extends Component {
 				selectedVideo: null
 			};
 
-			// fetch youtube videos
-			YTSearch({key: API_KEY, term: 'react js'}, (videos) => {
-				this.setState({
-					videos,
-					selectedVideo: videos[0]
-				});
+			this.videoSearch('react js');
+
+	}
+
+	videoSearch(term) {
+		// fetch youtube videos
+		YTSearch({key: API_KEY, term}, (videos) => {
+			this.setState({
+				videos,
+				selectedVideo: videos[0]
 			});
+		});
 	}
 
 	render() {
 		return (
 			<div className="search-bar">
-				<SearchBar />
+				<SearchBar
+				 	onSearchTermChange={(term) => this.videoSearch(term)} />
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList
 					onVideoSelect={selectedVideo => this.setState({selectedVideo})}

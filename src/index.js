28072c86200d3aa33ea5e136432fa74bebe2f38a
D'ontreye Nero <dontreyenero@gmail.com>
2017-02-05 16:14:54 -0500

    Add styling to center search bar

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ class App extends Component {
 
 	render() {
 		return (
-			<div>
+			<div className="search-bar">
 				<SearchBar />
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList
